b18dfe444821397de252aa7b876f68889b8899f4
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-07-28 10:14:03 -0400

    git pull script

---
+++ repoupdate.sh
@@ -0,0 +1,19 @@
+for REPO in `ls`; do
+if [ ${REPO: -3} == ".sh" ]; then
+echo 'This is a shell script.'
+else
+   (cd "$REPO";
+    if git rev-parse --is-inside-git-dir  > /dev/null 2>&1; then
+        if [[ $(git pull | grep 'Already up-to-date.') ]]; then
+            echo "Already up to date my man!"
+        else bundle && powder down && powder up;
+        fi
+    else
+	echo 'Not a git Repo.'
+    fi) 
+fi 
+done;
+
+cd domain-values; powder down && powder up; cd ../consumer-domain; powder down && powder up; cd ../consumer-main; powder down && powder up; 
+
+
